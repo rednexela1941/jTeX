649f7b4e07124a19fca55b7cdf2dbe4b429cd7e7
rednexela1941 <red@git.com>
2020-04-07 18:20:35 -0600

    Added in env and command

--- jtex.jl
+++ jtex.jl
@@ -1,9 +1,56 @@
 module jtex
 
-export document
+export document, env, command
+
+function command(type,content=nothing,options=nothing)
+    lines = []
+    if content != nothing
+        push!(lines, content)
+    end
+    function get_raw()
+        string = "\\$(type)"
+        if options != nothing
+            string *= "[$(options)]"
+        end
+        string *= "{"
+        for c in lines
+            if typeof(c) == String
+                string *= c
+            else
+                string *= c.get_raw()
+            end
+        end
+        string *= "}"
+    end
+    add(content) = push!(lines, content)
+    return ()->(add, get_raw)
+end
+
+function env(type, options=nothing) ## ie \begin{type}
+    contents = []
+    add(env) = push!(contents, env)
+    function get_raw()
+        string = "\\begin{$(type)}"
+        if options != nothing
+            string *= "[$(options)]"
+        end
+        string *= "\n"
+        for c in contents
+            if typeof(c) == String
+                string *= c * "\n"
+            else
+                string *= c.get_raw()
+            end
+        end
+        string *= "\\end{$(type)}"
+        return string
+    end
+    # create the environment, allow the addition of statments and sub environments.
+    return ()->(add, get_raw)
+end
 
 function document()
-    class = ("extrarticle", "12pt")
+    class = ("extarticle", "12pt")
     packages = []
     preamble = [] # comes after packages
     commands = []
@@ -22,8 +69,8 @@ function document()
     end
     statements = []
     # Relating to the document.
-    add_line(string) = push!(statements, string)
-
+    add(content) = push!(statements, content)
+    #This should really be a new environment here, then we will break things up in a more normal way.
     out_class() = begin
         if class != nothing
             return "\\documentclass[$(class[2])]{$(class[1])}\n"
@@ -35,7 +82,11 @@ function document()
         string = ""
         for (pkg, opts) in packages
             # println(pkg, opts)
-            string *= "\\usepackage[$(opts)]{$(pkg)}\n"
+            if opts != nothing
+              string *= "\\usepackage[$(opts)]{$(pkg)}\n"
+            else
+              string *= "\\usepackage{$(pkg)}\n"
+            end
         end
         return string
     end
@@ -50,7 +101,11 @@ function document()
     out_lines() = begin
         string = ""
         for c in statements
-            string *= c * "\n"
+            if typeof(c) == String
+                string *= c * "\n"
+            else # so it is a funciton
+                string *= c.get_raw()
+            end
         end
         return string
     end
@@ -75,7 +130,7 @@ function document()
         println("jtex wrote $(filename)")
     end
   # return the various functions.
-  return ()->(set_class, use_package, raw_preamble, new_command, add_line, generate_tex, output_tex)
+  return ()->(set_class, use_package, raw_preamble, new_command, add, generate_tex, output_tex)
 end
 
 # doc = document()
